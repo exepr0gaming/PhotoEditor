8a67bb31e267ac377bef4d9204b5cb8e75076eb1
exepr0gaming <exehomework@gmail.com>
2023-07-30 02:37:17 +0300

    Initial Commit

---
+++ PhotoEditor/PhotoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorApp.swift
+//  PhotoEditor
+//
+//  Created by Andrew Kurdin on 30.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
